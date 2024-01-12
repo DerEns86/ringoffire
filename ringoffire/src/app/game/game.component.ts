f4026136cc4c51c498eaa7b53a0e230f2fd84c49
Viktor Ens <v.ens86@yahoo.de>
2024-01-12 19:29:17 +0100

    cards and animation

--- ringoffire/src/app/game/game.component.ts
+++ ringoffire/src/app/game/game.component.ts
@@ -1,24 +1,51 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule , GameComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+export class GameComponent implements OnInit{
   pickCardAnimation = false;
-  
+  currentCard: string = '';
+  game!: Game;
   constructor() {
+   
+    
 
   }
 
+ngOnInit(): void {
+   this.newGame();
+}
+
+newGame(){
+ this.game = new Game();
+ console.log(this.game);
+}
 
   takeCard() {
-    console.log('Works');
-    this.pickCardAnimation = true;
+     if(!this.pickCardAnimation){   
+    let poppedCard = this.game.stack.pop();
+  if (poppedCard !== undefined) {
+    this.currentCard = poppedCard;
+    
+    console.log(this.currentCard);
+    console.log('played:' + this.game.playedCard);
+  } else {
+    console.log('Keine Karte mehr im Stapel.');
   }
+    this.pickCardAnimation = true;
 
+    setTimeout(() => {
+      this.game.playedCard.push(this.currentCard)
+      this.pickCardAnimation = false;
+
+    }, 1000);
+  }
+  }
 }
